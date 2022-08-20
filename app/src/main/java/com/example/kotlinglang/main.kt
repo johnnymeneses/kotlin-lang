81adfdad80400ece05a66d49dc7aeea7d107a68b
Johnny Meneses <bilbo_vader@outlook.com>
2022-08-20 17:36:24 -0300

    Projeto

---
+++ app/src/main/java/com/example/kotlinglang/main.kt
@@ -0,0 +1,64 @@
+import d_exercicios_logica.*
+
+fun main() {
+
+    var exercicio = 16
+
+
+    //Logica para chamar os exercícios
+    when (exercicio) {
+        1 -> Ex01(5)                                 //Tabuada
+        2 -> Ex02(2, 5)                             //Area do Terreno
+        3 -> println(Ex03(5))                        //Par ou impar?
+        4 -> println(Ex04(10, 5))                   // Divisao de dois numeros
+        5 -> Ex05(10, 11)                       //Padaria
+        6 -> println(Ex06("Johnny", 37))      //Idade em anos
+        7 -> println(Ex07(5.69f, 5.69f))     //Preço Gasolina
+        8 -> Ex08()
+        9 -> Ex09(10,1)                             //Quantos dias se passaram?
+        10-> Ex10(5,8,9)                    //Media Ponderada
+        11-> Ex11(5,8,9)                    //Fabrica de Camisetas
+//        12 >    Numeros Amigos
+        13 -> Ex13()                                           //Exemplo de entrada de dados usando o readLine()
+        14 -> Ex14(5000.0)                            //Aumento de Salario
+//        15
+        16 -> Ex16(15)
+//        17
+//        18
+//        19
+        20 -> Ex20(15, 20)          //Horas Extras
+        23 -> Ex23(1,1,0)               //qtosLitros
+        25 -> Ex25("Pneu",100.0,10.0)   //Desconto
+        40 -> Ex40()                                        //Exercicio For - Udemy
+        41 -> Ex41()                                        //Exercicio For - Udemy
+        42 -> Ex42()                                        //Exercicio For - Udemy
+        43 -> Ex43()                                        //Exercicio For Somatoria - Udemy
+        44 -> Ex44(5)                                   //Exercicio For #### - Udemy
+        45 -> Ex45()                                   //Exercicio doWhile - Udemy
+        46 -> Ex46()                                   //Exercicio doWhile - FizzBuzz
+
+    }
+
+
+}
+
+
+/*
+     Basic syntax
+    Package definition and imports
+    Program entry point
+    Print to the standard output
+    Functions
+    Variables
+    Creating classes and instances
+    Comments
+    String templates
+    Conditional expressions
+    for loop
+    while loop
+    when expression
+    Ranges
+    Collections
+    Nullable values and null checks
+    Type checks and automatic casts
+* */
